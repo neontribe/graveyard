459a5bc70e0fc42856cb01584220854dea5662f4
Toby Batch <tobias@neontribe.co.uk>
2015-05-28 08:34:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ while [ -z "$BR" ]; do
     read BR
 done
 
-git clone https://github.com/neontribe-ansible/cottage ansible-$BR
+git clone https://github.com/neontribe-ansible/cottage-PAS ansible-$BR
 cd ansible-$BR
 git submodule init
 git submodule update
