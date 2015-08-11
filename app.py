f0a447d6c5ae2d86c28b0fb9506c16f0f25b2222
Hoolean <me@hoolean.com>
2015-08-11 13:34:00 +0100

    Added static content serving

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
 
 from flask import Flask
+from flask import render_template
 
 import tweak
 
 app = Flask(__name__)
 server_config = tweak.load_config(os.path.sep.join([tweak.get_script_directory(), 'config', 'server']))
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(server_config['host'], server_config['port'], server_config['threading'])
