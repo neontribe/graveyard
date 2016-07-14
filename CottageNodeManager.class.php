2d68c251c45508532d95c59bf2685c7fbad99649
Oliver Barnwell <blastdoors666@gmail.com>
2016-07-14 16:43:55 +0100

    Add field creation routines

--- CottageNodeManager.class.php
+++ CottageNodeManager.class.php
@@ -2,20 +2,48 @@
 
 
 class CottageNodeManager {
-	var $cottage_fields = array(
-		'reference' => array(
-			'field_name' => 'cottage_reference',
-			'type' => 'text',
+	public static function registerCottageFieldDefinitionInstances($name, $cottage_fields) {
+		foreach ($cottage_fields as $field_key => $field_options) {
+			if(field_info_field($field_key)) {
+				continue;
+			}
 
-		),
-		'brandcode' => array(
-			'field_name' => 'cottage_brandcode',
-			'type' => 'text',
-		),
+			$field_options = field_create_field($field_options);
+			
+			$instance = array(
+				'field_name' => $field_key,
+				'entity_type' => 'node',
+				'bundle' => $name,
+				'description' => '.',
+				'label' => $field_key,
+				'widget' => array(
+					'type' => 'textfield',
+				) 
+			);
 
-	);
+			field_create_instance($instance);
+		}
+	}
 
-	public static function generateTypeDefinitionArray($name) {
+
+
+	public static function nodeTypeExists($name) {
+		#Check to see if cottage_node type exists
+		if ( in_array( $name, node_type_get_names() ) ) {
+    		return TRUE;
+		}
+
+		return FALSE;
+	}
+
+	public static function registerCottageNodeTypeEntity($name = 'cottage_entry') {
+		$cottage_type_defintion_array = self::generateTypeDefinitionArray($name);
+
+		node_type_save($cottage_type_defintion_array);
+		node_add_body_field($cottage_type_defintion_array);
+	}
+
+	private static function generateTypeDefinitionArray($name) {
 		#Ascertain whether the node type currently is defined in the database.
 		if(self::nodeTypeExists($name)) {
 			return FALSE;
@@ -37,21 +65,4 @@ class CottageNodeManager {
 
 		return $nt2_node_type;
 	}
-
-	public static function nodeTypeExists($name) {
-		#Check to see if cottage_node type exists
-		if ( in_array( $name, node_type_get_names() ) ) {
-    		return TRUE;
-		}
-
-		return FALSE;
-	}
-
-	public static function registerCottageNodeTypeEntity($name = 'cottage_entry') {
-		$cottage_type_defintion_array = self::generateTypeDefinitionArray($name);
-
-		node_type_save($cottage_type_defintion_array);
-		node_add_body_field($cottage_type_defintion_array);
-	}
-
 }
