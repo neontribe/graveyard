8803b4aba8f4e904187bf6595c5676b21b8087aa
Toby Batch <tobias@neontribe.co.uk>
2016-07-27 08:47:05 +0100

    Demonstrates preprocess hooks

--- templates/nt2_node_type_address.tpl.php
+++ templates/nt2_node_type_address.tpl.php
@@ -6,5 +6,9 @@
 
 <div class="nt2_note_type_address">
   <?php /* echo implode("<br />\n", array_reverse(array_filter($address))); */ ?>
-  
+  <h3>Address</h3>
+  <?php echo isset($addr1) ? $addr1 . '</br>' : ''; ?>
+  <?php echo isset($addr2) ? $addr2 . '</br>' : ''; ?>
+  <?php echo isset($county) ? $county . '</br>' : ''; ?>
+  <?php echo isset($postcode) ? $postcode . '</br>' : ''; ?>
 </div>
