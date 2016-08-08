34dd34b2f06ef3d564aefa431557b2c9c9ecd50a
Hoolean <me@hoolean.com>
2016-08-08 16:57:22 +0100

    Added a checkbox search term that is automatically generated from the attribute searchTerms, in addition to an infinite number of TODO comments

---
+++ nt2_search/NT2CheckboxSearchTerm.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * A basic SearchTerm, used to obtain a boolean value, represented by a checkbox in the form.
+ */
+class NT2CheckboxSearchTerm extends NT2SearchTerm {
+  /**
+   * Constructor overridden as CheckboxSearchTerm only covers one code, not multiple.
+   * TODO: consider adding checking of this
+   */
+  public function __construct($code, $defaultLabel) {
+    parent::__construct([$code], $defaultLabel);
+  }
+
+  public function injectInputs(&$form) {
+    // inject an HTML checkbox input
+    $form[$this->getName()] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->getLabel(), // TODO: consider passing this through t()
+    );
+  }
+
+  public function injectParams(&$params) {
+    // extract the value from the get request
+    $formValue = filter_input(INPUT_GET, $this->getName());
+
+    //////// OLD METHOD THAT DOES NOT OMIT ////////
+    // http checkbox to boolean string conversion
+    //$queryValue = ($formValue == '1') ? 'true' : 'false';
+
+    // inject the boolean string value into the parameter
+    // one can assume that there is only one code, owing to the constructor
+    //$params[$this->getIds()[0]] = $queryValue;
+    ///////////////////////////////////////////////
+
+    // omit from query if checkbox is unticked
+    // TODO: potentially make this optional or clever or something
+    if ($formValue == '1') {
+      $params[$this->getIds()[0]] = 'true';
+    }
+  }
+}
--- nt2_search/NT2Search.class.php
+++ nt2_search/NT2Search.class.php
@@ -7,16 +7,22 @@
  */
 
 class NT2Search {
-  
+  // TODO: consistent capitalisation pls
   
   public static function quickSearchForm() {
     $form = array();
 
     // inject input elements
-    $searchTerms = getSearchTerms(true); // get all enabled search terms
+    $searchTerms = NT2Search::getSearchTerms(true); // get all enabled search terms
     foreach($searchTerms as &$searchTerm) {
-      $searchTerm.injectInputs($form);
+      $searchTerm->injectInputs($form);
     }
+
+    // TODO: primitive check for name clashes
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Search'),
+    );
 
     return $form;
   }
@@ -36,21 +42,24 @@ class NT2Search {
   }
   
   public static function page() {
+    // TODO: this page can be rendered even if not directed from the form
+
     $params = array();
 
     // extract search queries
-    $searchTerms = getSearchTerms(true); // get all enabled search terms
+    $searchTerms = NT2Search::getSearchTerms(true); // get all enabled search terms
     foreach($searchTerms as &$searchTerm) {
-      $searchTerm.injectParams($params);
+      $searchTerm->injectParams($params);
     }
     
     $api = NeontabsIO::getInstance();
-    $json = $api->get('/property', $params);
+    $json = $api->get('property', $params);
         
     $render_array = array();
     
     // TODO: $json['results'] will not exist in the event of the API returning an error
     // at the moment this is not accounted for
+    dpm($json);
     foreach ($json['results'] as $property) {
 //      $node = CottageNodeManager::fetchPropertyFromAPI($property['propertyRef'], '_ZZ');
 //      $_render_array = nt2_node_type_node_render_array($node);
@@ -65,11 +74,23 @@ class NT2Search {
     return $render_array;
   }
 
-  public function getSearchTerms($onlyEnabled = false) {
+  public static function getSearchTerms($onlyEnabled = false) {
     // TODO: handle onlyEnabled, including dependency checks, etc
-    // TODO: parse JSON and work 'em all out
+
+    $searchTerms = array();
+
     // TODO: caching, is hitting the API everytime necessary?
-    // TODO: everything
-    return array();
+    $api = NeontabsIO::getInstance();
+    $json = $api->get('/'); // hit up the root API - TODO: this seems to return NULL in some scenarios
+    $json = $json['constants']['searchTerms']; // we only care about searchTerms - TODO: check this doesn't have horrific performance implications
+    
+    // TODO: handle custom "core" search terms
+
+    // make a CheckboxSearchTerm for every attribute
+    foreach ($json['attributes'] as $attribute) {
+      $searchTerms[] = new NT2CheckboxSearchTerm($attribute['code'], $attribute['label']);
+    }
+
+    return $searchTerms;
   }
 }
