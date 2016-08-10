76bc1b6688f0c5d8ff83358f6701043b4cc7071c
Oliver Barnwell <barnwelloliver@gmail.com>
2016-08-10 13:08:54 +0100

    Add teaser render array

--- nt2_node_type/templates/nt2_node_type_accommodation.tpl.php
+++ nt2_node_type/templates/nt2_node_type_accommodation.tpl.php
@@ -6,7 +6,7 @@
 
 <div class="nt2_node_type_accommodation">
   <?php /* echo implode("<br />\n", array_reverse(array_filter($address))); */ ?>
-  <h3>Accommodation Info</h3>
+  <!-- <h3>Accommodation Info</h3> -->
   <ul class="stacked accommodation-details">
 	  <li> <span>Sleeps:</span> <strong> <?php echo isset($accommodates) ? $accommodates : ''; ?> </strong></li>
 	  <li> <span>Bedrooms:</span> <strong> <?php echo isset($bedrooms) ? $bedrooms : ''; ?> </strong></li>
