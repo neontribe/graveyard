f5845ab9cb66e108b8ecd0f37eba1ddfe2649039
Oliver Barnwell <barnwelloliver@gmail.com>
2016-08-11 21:11:35 +0100

    Teaser work

--- template.php
+++ template.php
@@ -1,51 +1,55 @@
 <?php
 
-// function nt2_theme_field($variables) {
-// 	$output = "";
-
-// 	//Render the label, if it's not hidden.
-// 	// if (!$variables['label_hidden']) {
-// 	//  $output .= '<div class="field-label"' . $variables['title_attributes'] . '>' . $variables['label'] . ':&nbsp;</div>';
-// 	// }
-
-
-// 	// Render the items.
-// 	$output .= '<div class="field-items"' . $variables['content_attributes'] . '>';
-// 	foreach ($variables['items'] as $delta => $item) {
-// 		$classes = 'field-item ' . ($delta % 2 ? 'odd' : 'even');
-// 		$output .= '<div class="' . $classes . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</div>';
-// 	}
-// 	$output .= '</div>';
-
-// 	// Render the top-level DIV.
-// 	$output = '<div class="' . $variables['classes'] . '"' . $variables['attributes'] . '>' . $output . '</div>';
-
-// 	return $output;
-// }
-
-
 function nt2_theme_preprocess_field(&$vars) {
-  if ($node = menu_get_object()) {
-   
+  // if ($node = menu_get_object()) {
+  if ($node = $vars['element']['#object']) {
+
     if ($node->type == 'cottage_entity') {
-    	//kpr();
+    	$item_ref =& $vars['items'];
+    	$label = $vars['element']['#field_name'];
+
     	
-    	// $element =& $vars['element'];
-
-    	// $label = $vars['element']['#field_name'];
     
-    	// kpr($vars['theme_hook_suggestions']);
+    	if($label == 'cottage_images') {
+    		$image_rndarray = array(
+				'cottage-images' => array(
+					'#prefix' => '<ul>',
+					'#suffix' => '</ul>',
+				),
+			);
 
-    	// if($label == 'cottage_reference') {
+			$index = 0;
+
+			foreach ($node->cottage_images as $image) {
+				list($alt, $title, $url) = explode("\n", $image['value']);
+
+				$_image = array(
+				 '#prefix' => '<li>',
+				 '#suffix' => '</li>',
+				 '#theme' => 'imagecache_external',
+				 '#path' => $url,
+				 '#style_name' => 'thumbnail',
+				 '#alt' => $alt,
+				 '#title' => $title,
+				);
+
+			 	$image_rndarray['cottage-images']['image' . sprintf('%02d', $index++)] = $_image;
+			}
+
+    		$item_ref = $image_rndarray;
+    	
     		
-    	// 	// $item_ref = array(
-    	// 	// 	0 => array(
-    	// 	// 		'#prefix' => '<h2>',
-    	// 	// 		'#suffix' => '</h2>',
-    	// 	// 		'#markup' => "Foo Bar",
-    	// 	// 	),
-    	// 	// );
-    	// }
+    	} else {
+    		$item_ref = array(
+				0 => array(
+					'#prefix' => '<h2>',
+					'#suffix' => '</h2>',
+					'data' => $item_ref,
+				),
+			);
+    	}
+		
+	
 
     }
 
--- templates/field--cottage-entity.tpl.php
+++ templates/field--cottage-entity.tpl.php
@@ -1,12 +1,12 @@
 
-<div class="property-fields">
+<div class="property-field">
 
 <?php 
 
 foreach ($variables['items'] as $delta => $item) {
 	$rendered_item = drupal_render($item);
 
-	$output = '<div class="field-item' . $delta . '">' . $rendered_item . '</div>';
+	$output = '<div class="field-item-' . $delta . '">' . $rendered_item . '</div>';
 
 	print $output;
 } 
