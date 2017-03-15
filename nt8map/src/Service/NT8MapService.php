c465ad44e9273807fe1d8cc7072880f168325ea3
Oliver Barnwell <barnwelloliver@gmail.com>
2017-03-15 16:33:57 +0000

    add new map service

---
+++ nt8map/src/Service/NT8MapService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\nt8map;
+
+/**
+ * Class NT8MapService.
+ *
+ * @package Drupal\nt8map
+ */
+class NT8MapService {
+
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+
+  }
+
+}
