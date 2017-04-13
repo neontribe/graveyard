94b14b4f6cc5802c1cdc2dcc34d97e02312276cd
Oliver Barnwell <barnwelloliver@gmail.com>
2017-04-13 23:33:41 +0100

    More improvements to the theme
    New teaser layout; not responsive yet

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 (function($) {
+    // Settings
+    var options = {
+        scrollOffset: 20
+    };
+
+    // Header.
+    var $header = $('header[role=banner]');
+    var headerHeight = $header.height() || 0;
+
+    // Left sidebar.
+    var $leftSidebar = $('.region-sidebar-left > .content-wrap');
+
     // Sticky sidebar.
-    $(".region-sidebar-left > .content-wrap").stick_in_parent({
-        offset_top: 20
+    $leftSidebar.stick_in_parent({
+        offset_top: options.scrollOffset
     });
+
 })(jQuery);
