522475e0d0aaf6d3c2049c56694f651ed4ce1927
Oliver Barnwell <barnwelloliver@gmail.com>
2017-04-14 16:47:54 +0100

    Slider options

--- js/slick.config.js
+++ js/slick.config.js
@@ -7,24 +7,26 @@
                 dots: true,
                 infinite: true,
                 speed: 1000,
+                autoplay: true,
+                autoplaySpeed: Math.random()*10000 + 2000,
                 slidesToShow: 1,
                 prevArrow: '<button type="button" class="slick-prev"></button>',
-                nextArrow: '<button type="button" class="slick-next"></button>',
-                responsive: [
-                    {
-                        breakpoint: 1600,
-                        settings: {
-                            centerPadding: '300px',
-                        }
-                    },
-                    {
-                        breakpoint: 960,
-                        settings: {
-                            adaptiveHeight: true,
-                            centerPadding: '100px',
-                        }
-                    },
-                ]
+                nextArrow: '<button type="button" class="slick-next"></button>'
+                // responsive: [
+                //     {
+                //         breakpoint: 1600,
+                //         settings: {
+                //             centerPadding: '300px',
+                //         }
+                //     },
+                //     {
+                //         breakpoint: 960,
+                //         settings: {
+                //             adaptiveHeight: true,
+                //             centerPadding: '100px',
+                //         }
+                //     },
+                // ]
             });
 
         }
