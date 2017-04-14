227085a3a316c22c2a07f71d13fbe42f0acc59c2
Oliver Barnwell <barnwelloliver@gmail.com>
2017-04-14 16:27:05 +0100

    Add property slider js and styles

---
+++ js/slick.config.js
@@ -0,0 +1,33 @@
+(function ($, Drupal, window, document) {
+
+    Drupal.behaviors.THEMESlickConfig = {
+        attach: function (context, settings) {
+
+            $(".js-slick-single-item-center").slick({
+                dots: true,
+                infinite: true,
+                speed: 1000,
+                slidesToShow: 1,
+                prevArrow: '<button type="button" class="slick-prev"></button>',
+                nextArrow: '<button type="button" class="slick-next"></button>',
+                responsive: [
+                    {
+                        breakpoint: 1600,
+                        settings: {
+                            centerPadding: '300px',
+                        }
+                    },
+                    {
+                        breakpoint: 960,
+                        settings: {
+                            adaptiveHeight: true,
+                            centerPadding: '100px',
+                        }
+                    },
+                ]
+            });
+
+        }
+    };
+
+} (jQuery, Drupal, this, this.document));
