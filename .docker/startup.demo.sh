3c46ad1e7aa430b980a7d7e9b54b858c77fe8b08
Toby Batch <tobias@neontribe.co.uk>
2017-06-07 21:20:17 +0100

    Added seperate docker files for tester and demo images'

---
+++ .docker/startup.demo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+checkout.sh
+
+drush \
+    -r /opt/nt8/web \
+    -y site-install \
+    --db-url=sqlite://sites/default/files/.ht.sqlite \
+    --account-mail=cottages@neontribe.co.uk \
+    --account-name=superadmin \
+    --site-mail=cottages@neontribe.co.uk \
+    --site-name=EntyAte
+drush -r /opt/nt8/web -y en nt8theme
+drush -r /opt/nt8/web -y config-set system.theme default nt8theme
+drush -r /opt/nt8/web -y en nt8property
+drush -r /opt/nt8/web -y en nt8map nt8search
+drush -r /opt/nt8/web -y nt8-import-all
+compass compile /opt/nt8/web/themes/contrib/nt8theme/
+RUNSERVER_BASE_URL='http://localhost:8888' drush -r /opt/nt8/web rs 0.0.0.0:8888
