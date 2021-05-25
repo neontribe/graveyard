988720b1c9f7de0dc0c2907bcbeddd6f7abec48e
Toby Batch <tobias@neontribe.co.uk>
2021-05-25 09:30:12 +0100

    Moved .docker files

--- .docker/startup.tester.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-checkout.sh
-
-if [ ! -z "${PHPUNIT_GROUP}" ]; then
-    /opt/nt8/vendor/bin/phpunit --group ${PHPUNIT_GROUP} --configuration /opt/nt8/web/core/phpunit.xml.dist
-else
-    /opt/nt8/vendor/bin/phpunit --configuration /opt/nt8/web/core/phpunit.xml.dist
-fi
