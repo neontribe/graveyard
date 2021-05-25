7d47e047c9690d058ae5c4d48f0049dd7c9c9568
Toby Batch <tobias@neontribe.co.uk>
2021-05-25 08:53:53 +0100

    Prep for graveyard

---
+++ nt8theme/README.md
@@ -0,0 +1,4 @@
+# nt8_theme
+Base Drupal 8 Theme For The NT8 Project.
+
+[NT8 Repo](https://github.com/neontribe/nt8)
---
+++ nt8theme/js/slick.config.js
@@ -0,0 +1,58 @@
+(function ($, Drupal, window, document) {
+
+    Drupal.behaviors.THEMESlickConfig = {
+        attach: function (context, settings) {
+            var asNavSelector = null;
+            if($('body').hasClass('page-node-type-property')) {
+                asNavSelector = '.field--name-cottage-images-control ul';
+            }
+
+            $(".field--name-cottage-images ul")
+                .on('init', function(slick) {
+                    // debugger;
+                    $(".field--name-cottage-images ul").fadeIn(3000);
+                })
+                .slick({
+                    infinite: true,
+                    speed: 1000,
+                    autoplay: true,
+                    autoplaySpeed: Math.random()*10000 + 2000,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    prevArrow: '<button type="button" class="slick-prev"></button>',
+                    nextArrow: '<button type="button" class="slick-next"></button>',
+                    mobileFirst: true,
+                    centerMode: false,
+                    variableWidth: false,
+                    focusOnSelect: true,
+                    arrows: true,
+                    asNavFor: asNavSelector
+                }
+            );
+
+            $('.field--name-cottage-images-control ul').slick({
+                focusOnSelect: true,
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                asNavFor: '.field--name-cottage-images ul',
+                arrows: false,
+                vertical: true,
+                centerMode: true,
+                verticalSwiping: true,
+                responsive: [
+                    {
+                        breakpoint: 1400,
+                        settings: {
+                            centerMode: false,
+                            verticalSwiping: false,
+                            vertical: false,
+                            slidesToShow: 3
+                        }
+                    },
+                ]
+            });
+
+        }
+    };
+
+} (jQuery, Drupal, this, this.document));
